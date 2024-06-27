a382bb1479dcc97d3760c28208329a9ef52d4290
tiennguyenxg <tien.nguyen.xg@renesas.com>
2024-06-27 17:49:27 +0700

    fix i2c project syntax

--- i2c_interface/i2c_helper.cpp
+++ i2c_interface/i2c_helper.cpp
@@ -18,18 +18,17 @@ I2C_Helper::~I2C_Helper()
 
 int I2C_Helper::i2c_open()
 {
-	QString filename = "/dev/i2c-3";
+    QString filename = I2C_DEVICE_FILE;
     this->file = open(filename.toStdString().c_str(), O_RDWR);
     if (this->file < 0) {
-		qWarning() << "Failed to open the i2c bus";
-		return -1;
-	}
-
-	int addr = 0x50;
-    if (ioctl(this->file, I2C_SLAVE, addr) < 0) {
-		qWarning() << "Failed to acquire bus access and/or talk to slave";
+        qWarning() << "Failed to open the i2c bus";
         return -1;
-	}
+    }
+
+    if (ioctl(this->file, I2C_SLAVE, I2C_DEVICE_ADDR) < 0) {
+        qWarning() << "Failed to acquire bus access and/or talk to slave";
+        return -1;
+    }
 
     return 1;
 }
@@ -39,17 +38,17 @@ int I2C_Helper::i2c_read(int base_address, int next_byte)
     char buf[1] = { static_cast<char>(base_address) };
 
     if (::write(this->file, buf, 1) != 1) {
-		qWarning() << "Failed to write to the i2c bus";
+        qWarning() << "Failed to write to the i2c bus";
         return -1;
-	}
+    }
 
-	char data[1] = { 0 };
+    char data[1] = { 0 };
     if (::read(this->file, data, next_byte) != 1) {
-		qWarning() << "Failed to read from the i2c bus";
+        qWarning() << "Failed to read from the i2c bus";
         return -1;
-	}
+    }
 
-	qDebug() << "Data read:" << QString::number(static_cast<unsigned char>(data[0]), 16);
+    qDebug() << "Data read:" << QString::number(static_cast<unsigned char>(data[0]), 16);
     return 1;
 }
 
@@ -62,10 +61,8 @@ int I2C_Helper::i2c_close()
 {
     if (this->file != -1) {
         close(this->file);
-        this->file = -1; // Reset file descriptor
+        this->file = -1;
     }
 
     return 1;
 }
-
-
